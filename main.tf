eec18053f7a25f641cb077c313ba208e5d509d16
manojpannala <manojpannala@outlook.com>
2021-05-19 12:00:52 +0300

    VPC and Subnet deployed in AWS

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 provider "aws" {
-  region = "eu-central-1"
+  region = var.avail_zone
+}
+
+variable vpc_cidr_block {
+  description = "vpc cidr block"
 }
 
 variable subnet_cidr_block {
   description = "subnet cidr block"
 }
-variable vpc_cidr_block {
-  description = "vpc cidr block"
-}
+
 variable avail_zone {}
 
 variable env_prefix {}
@@ -21,7 +23,7 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
+  vpc_id = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
   availability_zone = var.avail_zone
     tags = {
