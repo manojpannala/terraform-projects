5d0052fc051763da696d993cf2edbb0d15abd724
manojpannala <manojpannala@outlook.com>
2021-05-19 12:35:39 +0300

    Changed rtb to main rtb. aws_default_route_table

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
   route{
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
-    Name: "${var.env_prefix}-rtb"
+    Name: "${var.env_prefix}-main-rtb"
   }
 }
 
