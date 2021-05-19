8b5e98edebfd98523f931dcbd29bee503e0bffa1
manojpannala <manojpannala@outlook.com>
2021-05-19 11:56:47 +0300

    Added String Interpolation for name tagging

--- main.tf
+++ main.tf
@@ -10,12 +10,13 @@ variable vpc_cidr_block {
 }
 variable avail_zone {}
 
+variable env_prefix {}
 
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
@@ -24,6 +25,6 @@ resource "aws_subnet" "myapp-subnet-1" {
   cidr_block = var.subnet_cidr_block
   availability_zone = var.avail_zone
     tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-subnet-1"
   }
 }
