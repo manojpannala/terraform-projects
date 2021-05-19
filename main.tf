0e533da758db3690282381000bac1b4a034bc6ff
manojpannala <manojpannala@outlook.com>
2021-05-19 12:21:51 +0300

    Added Internet Gateway and added gateway_id to rtb

--- main.tf
+++ main.tf
@@ -35,10 +35,16 @@ resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
   route{
     cidr_block = "0.0.0.0/0"
-    # gateway_id = 
+    gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
     Name: "${var.env_prefix}-rtb"
   }
 }
 
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
